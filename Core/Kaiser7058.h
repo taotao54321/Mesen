48c80c19beb2ff8d019caaf91bb3687d591f51a9
negativeExponent <negativeExponent@users.noreply.github.com>
2022-03-16 18:57:01 +0800

    Mapper 171: Fixed CHR banking issue

--- Core/Kaiser7058.h
+++ Core/Kaiser7058.h
@@ -7,7 +7,7 @@ class Kaiser7058 : public BaseMapper
 protected:
 	virtual uint16_t GetPRGPageSize() override { return 0x8000; }
 	virtual uint16_t GetCHRPageSize() override { return 0x1000; }
-	virtual uint16_t RegisterStartAddress() override { return 0xF000; }
+	virtual uint16_t RegisterStartAddress() override { return 0x8000; }
 	virtual uint16_t RegisterEndAddress() override { return 0xFFFF; }
 
 	void InitMapper() override
@@ -17,9 +17,9 @@ protected:
 
 	void WriteRegister(uint16_t addr, uint8_t value) override
 	{
-		switch(addr & 0xF080) {
-			case 0xF000: SelectCHRPage(0, value); break;
-			case 0xF080: SelectCHRPage(1, value); break;
+		switch(addr & 0x01) {
+			case 0x00: SelectCHRPage(0, value); break;
+			case 0x01: SelectCHRPage(1, value); break;
 		}
 	}
 };
