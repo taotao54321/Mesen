f71c6424e64aff23aec54875279b633f9e91b404
negativeExponent <negativeExponent@users.noreply.github.com>
2022-03-24 15:21:19 +0800

    Mapper 219: Add outer bank register

--- Core/MMC3_219.h
+++ Core/MMC3_219.h
@@ -10,59 +10,79 @@ protected:
 	void InitMapper() override
 	{
 		MMC3::InitMapper();
-		SelectPrgPage4x(0, -4);
-		SelectChrPage8x(0, 0);
-		_exRegs[0] = _exRegs[1] = _exRegs[2] =  0;
+		AddRegisterRange(0x5000, 0x5FFF);
 	}
 
-	void UpdatePrgMapping() override
+	void Reset(bool softreset) override
 	{
+		_exRegs[0] = _exRegs[2] = 0x00;
+		_exRegs[1] = 0x03;
+		MMC3::UpdateState();
 	}
 
-	void UpdateChrMapping() override
+	void SelectCHRPage(uint16_t slot, uint16_t page, ChrMemoryType memoryType = ChrMemoryType::Default) override
 	{
+		MMC3::SelectCHRPage(slot, (_exRegs[1] << 7) | (page & 0x7F));
+	}
+
+	void SelectPRGPage(uint16_t slot, uint16_t page, PrgMemoryType memoryType = PrgMemoryType::PrgRom) override
+	{
+		MMC3::SelectPRGPage(slot, (_exRegs[1] << 4) | (page & 0x0F));
 	}
 
 	void WriteRegister(uint16_t addr, uint8_t value) override
 	{
-		if(addr < 0xA000) {
+		if(addr < 0x8000) {
+			switch(addr & 0x01) {
+				case 0: _exRegs[1] = (_exRegs[1] & ~0x01) | ((value >> 3) & 0x01); break;
+				case 1: _exRegs[1] = (_exRegs[1] & ~0x02) | ((value >> 4) & 0x02); break;
+			}
+			MMC3::UpdatePrgMapping();
+			MMC3::UpdateChrMapping();
+		} else if(addr < 0xA000) {
 			switch(addr & 0xE003) {
-				case 0x8000: 
-					_exRegs[0] = 0;
-					_exRegs[1] = value;
-					break;
+			case 0x8000:
+				MMC3::WriteRegister(addr, value);
+				break;
+			
+			case 0x8002:
+				_exRegs[0] = value;
+				MMC3::WriteRegister(addr, value);
+				break;
 
-				case 0x8001:
-					if(_exRegs[0] >= 0x23 && _exRegs[0] <= 0x26) {
+			case 0x8001:
+				if(_exRegs[0] & 0x20) { // Extended Mode
+					uint8_t bankRegister = MMC3::GetState().Reg8000;
+					if((bankRegister >= 0x25) && (bankRegister <= 0x26)) {
 						uint8_t prgBank = ((value & 0x20) >> 5) | ((value & 0x10) >> 3) | ((value & 0x08) >> 1) | ((value & 0x04) << 1);
-						SelectPRGPage(0x26 - _exRegs[0], prgBank);
+						MMC3::_registers[6 | (bankRegister & 0x01)] = prgBank;
+						MMC3::UpdatePrgMapping();
+					} else if((bankRegister >= 0x08) && (bankRegister <= 0x1F)) {
+						switch(bankRegister) {
+							case 0x08: case 0x0A: case 0x0E: case 0x12: case 0x16: case 0x1A: case 0x1E: 
+								_exRegs[2] = value << 4; 
+								break;
+
+							case 0x09: MMC3::_registers[0] = _exRegs[2] | (value >> 1 & 0x0E); break;
+							case 0x0B: MMC3::_registers[0] = _exRegs[2] | (value >> 1 | 0x1); break;
+							case 0x0C:
+							case 0x0D: MMC3::_registers[1] = _exRegs[2] | (value >> 1 & 0xE); break;
+							case 0x0F: MMC3::_registers[1] = _exRegs[2] | (value >> 1 | 0x1); break;
+							case 0x10:
+							case 0x11: MMC3::_registers[2] = _exRegs[2] | (value >> 1 & 0xF); break;
+							case 0x14:
+							case 0x15: MMC3::_registers[3] = _exRegs[2] | (value >> 1 & 0xF); break;
+							case 0x18:
+							case 0x19: MMC3::_registers[4] = _exRegs[2] | (value >> 1 & 0xF); break;
+							case 0x1C:
+							case 0x1D: MMC3::_registers[5] = _exRegs[2] | (value >> 1 & 0xF); break;
+						}
+						MMC3::UpdateChrMapping();
 					}
-
-					switch(_exRegs[1]) {
-						case 0x08: case 0x0A: case 0x0E: case 0x12: case 0x16: case 0x1A:	case 0x1E: 
-							_exRegs[2] = value << 4; 
-							break;
-
-						case 0x09: SelectCHRPage(0, _exRegs[2] | (value >> 1 & 0x0E)); break;
-						case 0x0B: SelectCHRPage(1, _exRegs[2] | (value >> 1 | 0x1)); break;
-						case 0x0C:
-						case 0x0D: SelectCHRPage(2, _exRegs[2] | (value >> 1 & 0xE)); break;
-						case 0x0F: SelectCHRPage(3, _exRegs[2] | (value >> 1 | 0x1)); break;
-						case 0x10:
-						case 0x11: SelectCHRPage(4, _exRegs[2] | (value >> 1 & 0xF)); break;
-						case 0x14:
-						case 0x15: SelectCHRPage(5, _exRegs[2] | (value >> 1 & 0xF)); break;
-						case 0x18:
-						case 0x19: SelectCHRPage(6, _exRegs[2] | (value >> 1 & 0xF)); break;
-						case 0x1C:
-						case 0x1D: SelectCHRPage(7, _exRegs[2] | (value >> 1 & 0xF)); break;
-					}
-					break;
-
-				case 0x8002:
-					_exRegs[0] = value;
-					_exRegs[1] = 0;
-					break;
+				} else { // Normal MMC3
+					MMC3::WriteRegister(addr, value);
+				}
+				break;
 			}
 		} else {
 			MMC3::WriteRegister(addr, value);
