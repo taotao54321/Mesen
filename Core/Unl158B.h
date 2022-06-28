38c28db254a9b8d16d276358c2bd15d27f888001
negativeExponent <negativeExponent@users.noreply.github.com>
2022-06-28 23:20:36 +0800

    Mapper 258: Fixed support for Super Hang-on

--- Core/Unl158B.h
+++ Core/Unl158B.h
@@ -1,12 +1,21 @@
 #pragma once
 #include "stdafx.h"
-#include "MMC3.h"
-#include "MemoryManager.h"
+#include "MMC3_215.h"
 
-class Unl158B : public MMC3
+class Unl158B : public MMC3_215
 {
 private:
-	const uint8_t _protectionLut[8] = { 0x00, 0x00, 0x00, 0x01, 0x02, 0x04, 0x0F, 0x00 };
+	const uint8_t _protectionLut[8][8] = {
+		{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },
+		{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00 },
+		{ 0x00, 0x00, 0x00, 0x00, 0x03, 0x04, 0x00, 0x00 },
+		{ 0x00, 0x00, 0x00, 0x01, 0x00, 0x04, 0x05, 0x00 },
+		{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },
+		{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },
+		{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },
+		{ 0x00, 0x00, 0x00, 0x01, 0x02, 0x04, 0x0F, 0x00 }
+	};
+
 	uint8_t _reg;
 
 protected:
@@ -14,54 +23,28 @@ protected:
 
 	void InitMapper() override
 	{
-		_reg = 0;
+		_reg = 7;
+		MMC3_215::InitMapper();
 		AddRegisterRange(0x5000, 0x5FFF, MemoryOperation::Any);
 		RemoveRegisterRange(0x8000, 0xFFFF, MemoryOperation::Read);
-		MMC3::InitMapper();
-	}
-
-	void Reset(bool softReset) override
-	{
-		_reg = 0;
-		ResetMmc3();
-		MMC3::Reset(softReset);
 	}
 
 	void StreamState(bool saving) override
 	{
-		MMC3::StreamState(saving);
+		MMC3_215::StreamState(saving);
 		Stream(_reg);
-	}
-
-	void SelectPRGPage(uint16_t slot, uint16_t page, PrgMemoryType memoryType = PrgMemoryType::PrgRom) override
-	{
-		if(_reg & 0x80) {
-			uint32_t bank = _reg & 0x07;
-			if(_reg & 0x20) {
-				SelectPrgPage4x(0, (bank & 0x06) << 1);
-			} else {
-				SelectPrgPage2x(0, bank << 1);
-				SelectPrgPage2x(1, bank << 1);
-			}
-		} else {
-			MMC3::SelectPRGPage(slot, page & 0x0F);
-		}
 	}
 
 	uint8_t ReadRegister(uint16_t addr) override
 	{
-		return _console->GetMemoryManager()->GetOpenBus() | _protectionLut[addr & 0x07];
+		return (_console->GetMemoryManager()->GetOpenBus() & 0xF0) | _protectionLut[_reg][addr & 0x07];
 	}
 
 	void WriteRegister(uint16_t addr, uint8_t value) override
 	{
-		if(addr <= 0x5FFF) {
-			if((addr & 0x07) == 0) {
-				_reg = value;
-				UpdatePrgMapping();
-			}
-		} else {
-			MMC3::WriteRegister(addr, value);
+		if((addr <= 0x5FFF) && ((addr & 0xF007) == 0x5002)) {
+			_reg = value & 0x07;
 		}
+		MMC3_215::WriteRegister(addr, value);
 	}
 };
